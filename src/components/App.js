8a206ce3a888372a69419d6c84b2c589352a2607
Ha young, Kim <jeremiah91@naver.com>
2021-05-07 14:20:47 +0900

    #error : cannot perform a React State update

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  // user 의 로그인, 로그아웃 상태 확인
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      // listening for the change :
+      // 1) user logged in
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing ..."}
       <footer> &copy; Hawitter {new Date().getFullYear()} </footer>
     </>
   );
