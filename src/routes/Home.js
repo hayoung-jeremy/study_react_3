ce7d1b233e02f879fa3380813c996346c95d4eea
Ha young, Kim <jeremiah91@naver.com>
2021-05-10 10:32:57 +0900

    creating Form and Database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,32 @@
 // main page
 
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [haweet, setHaweet] = useState();
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setHaweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={haweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="Haweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
