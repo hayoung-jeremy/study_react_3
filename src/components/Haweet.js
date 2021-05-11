9ef479ae9c75628cce1092394e7d54f25f1721d3
Ha young, Kim <jeremiah91@naver.com>
2021-05-11 22:12:36 +0900

    delete & edit haweets --2

--- src/components/Haweet.js
+++ src/components/Haweet.js
@@ -1,14 +1,53 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
 const Haweet = ({ haweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newHaweet, setNewHaweet] = useState(haweetObj.text);
+
+  const onDeleteClick = () => {
+    const ok = window.confirm("Are you sure want to delete this hawwet?");
+    console.log(ok);
+    if (ok) {
+      //delete
+      dbService.doc(`haweets/${haweetObj.id}`).delete();
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewHaweet(value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(haweetObj, newHaweet);
+    dbService.doc(`haweets/${haweetObj.id}`).update({
+      text: newHaweet,
+    });
+    setEditing(false);
+  };
   return (
     <div>
-      <h4> {haweetObj.text} </h4>
-      {/* 작성자일 경우에만 tweet 편집 및 삭제 가능 */}
-      {isOwner && (
+      {editing ? (
         <>
-          <button>delete haweet</button>
-          <button>edit haweet</button>
+          <form onSubmit={onSubmit}>
+            <input type="text" value={newHaweet} required onChange={onChange} />
+            <input type="submit" value="Update Haweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4> {haweetObj.text} </h4>
+          {/* 작성자일 경우에만 tweet 편집 및 삭제 가능 */}
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>delete haweet</button>
+              <button onClick={toggleEditing}>edit haweet</button>
+            </>
+          )}
         </>
       )}
     </div>
